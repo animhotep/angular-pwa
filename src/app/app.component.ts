7b43af4648944991d3611eb27582cd6da15cf255
pasha <animhotep@gmail.com>
2018-10-08 13:58:57 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-pwa';
+  constructor(private meta: Meta, private titleService: Title) {
+    this.meta.addTag({ name: 'description', content: 'Index page' });
+    this.meta.addTag({ name: 'author', content: 'pasha' });
+    this.meta.addTag({ name: 'keywords', content: 'angular, pwa' });
+    this.titleService.setTitle('Angular PWA install');
+  }
+  title = 'Angular PWA install page';
 }
